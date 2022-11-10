3a212ef2f957a76ec9c8f54d26f258310438b372
NEMESIS <inc.nemesis@rambler.ru>
2022-11-10 10:21:40 +0300

    Добавлен minus

--- ProJJJ/Hz.py
+++ ProJJJ/Hz.py
@@ -1,9 +1,5 @@
-import sys
-import numpy as np
-import cv2 as cv
-
-hsv_min = np.array((0, 54, 5), np.uint8)
-hsv_max = np.array((187, 255, 253), np.uint8)
+def minus(a,b):
+    return a-b
 
 if __name__ == '__main__':
     print(minus(4, 3))

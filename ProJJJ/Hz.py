b8e8b6dbe46e3abc2f1d01897027dbec9178e236
NEMESIS <inc.nemesis@rambler.ru>
2022-11-10 10:28:42 +0300

    Добавлен plus и nul

--- ProJJJ/Hz.py
+++ ProJJJ/Hz.py
@@ -1,5 +1,13 @@
 def minus(a,b):
     return a-b
 
+def nul(a,b):
+    return a*b
+
+def plus (a,b):
+    return a+b
+
 if __name__ == '__main__':
     print(minus(4, 3))
+    print(nul(3,9))
+    print(plus(2,3))

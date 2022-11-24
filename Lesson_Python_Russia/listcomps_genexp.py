6b250fd0c2f26ab9a91a22a3191720bbf4d3b2ee
NEMESIS <inc.nemesis@rambler.ru>
2022-11-24 18:21:47 +0300

    Добавил +2

--- Lesson_Python_Russia/listcomps_genexp.py
+++ Lesson_Python_Russia/listcomps_genexp.py
@@ -26,3 +26,4 @@ matrix = [list(range(x, x + 3)) for x in range(3)]
 
 if __name__ == '__main__':
     pprint.pprint(matrix, indent=1, width=15)
+print()
--- ProJJJ/Hz.py
+++ ProJJJ/Hz.py
@@ -1,13 +1,4 @@
-def minus(a,b):
-    return a-b
-
-def nul(a,b):
-    return 2+a*b
-
-def plus (a,b):
-    return a+b
-
-if __name__ == '__main__':
-    print(minus(4, 3))
-    print(nul(3,9))
-    print(plus(2,3))
+start = 'Na' * 4
+middle = 'Hey ' * 3 + '\n'
+end = 'Good Bye!'
+print(start + start + middle + end)

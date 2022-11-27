1098ec1bba365b93c9e7518aca068c6829af9c8b
NEMESIS <inc.nemesis@rambler.ru>
2022-11-27 12:49:39 +0300

    добавили

--- Lesson_Python_Russia/listcomps_genexp.py
+++ Lesson_Python_Russia/listcomps_genexp.py
@@ -5,7 +5,16 @@ import pprint
 # читается слева направо.
 # [ВЫРАЖЕНИЕ/ПРЕОБРАЗОВЫВАНИЕ for element in ИСТОЧНИК if УСЛОВИЕ]
 # Нельзя делать больше 2х for в одной строке[].
+# Для соваря обязательно указать КЛЮЧ:ЗНАЧЕНИЕ (двоеточие)
+# Genexp. Генератор не формирует структуру данных,
+# как это делают ...comps, он формирует ОБЪЕКТ генератора.
+# Генератор вернёт объект, а не коллекцию
+# Генератор ленивый, не выполяется и не занимает память, пока не потребуется.
+# Генератор проверяет источник при создании!!!
+# Генератор одноразовый, если исчерпан, то бросает StopIteration.
+# цикл for перехватывает StopIteration.
 
+from time import sleep
 
 text = 'hello world'
 words = [word.capitalize() for word in text.split()]  # только преобразовываем
@@ -20,9 +29,34 @@ for e in range(10):
     if e % 2 == 0:
         squares2.append(e * e)
 
-letters = [letter for word in text.split() for letter in word if letter > 'l']
-
 matrix = [list(range(x, x + 3)) for x in range(3)]
 
+# Listcomps
+letters = [letter for word in text.split() for letter in word if letter > 'l']
+
+# Setcomps, создаёт 'множества'.
+unique_letters = {letter for word in text.split() for letter in word if letter < 'o'}
+
+# Dcitcomps. Словарь.
+alphabet = {index: letter for index, letter in enumerate('absdifghijklnopqrstuvwxyz', 1)}
+
+# Genexp. Генератор не формирует структуру данных, как это делают ...comps
+# он формирует ОБЪЕКТ генератора.
+positives_gen = (e for e in ints if e > 0)
+
+
+def some_sourse():
+    # open db
+    # read file
+    # calculate
+    print("!!!")
+    sleep(3)
+    return [1, 2, 3]
+
+
 if __name__ == '__main__':
-    pprint.pprint(matrix, indent=1, width=15)
+    for e in positives_gen:
+        print(e)
+    print("===============")
+    for e in positives_gen:
+        print(e)

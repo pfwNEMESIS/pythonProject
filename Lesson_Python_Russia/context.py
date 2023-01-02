bb75e0f1144025053c7a9f498fca771010954f39
pfwNEMESIS <inc.nemesis@rambler.ru>
2023-01-02 19:22:32 +0300

    Добавил программу contex полностью.

--- Lesson_Python_Russia/context.py
+++ Lesson_Python_Russia/context.py
@@ -1,3 +1,6 @@
+from contextlib import contextmanager
+
+
 class Resource:
     def __init__(self):
         self.opened = False
@@ -18,5 +21,36 @@ class Resource:
         print('Do something with resource')
 
 
+@contextmanager
+def open_resource(*args):
+    resource = None
+    try:
+        resource = Resource()
+        resource.open(*args)
+        yield resource
+    except:
+        raise
+    finally:
+        if resource:
+            resource.close()
+
+
+class ResourceWorker:
+    def __init__(self, *args):
+        self.args = args
+        self.resource = None
+
+    def __enter__(self):
+        self.resource = Resource()
+        self.resource.open(*self.args)
+        return self.resource
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.resource:
+            self.resource.close()
+
+
 if __name__ == '__main__':
-    pass
+    with ResourceWorker(1, 2, 3) as res:
+        res.action()
+        raise ValueError("Stop")
